0791cd24ff86115bc1b29a73fdd63760d391c531
rhob-weber <rhob.weber@procode.co.uk>
2024-12-12 04:55:05 -0500

    Move btn get to web_steps

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -60,11 +60,6 @@ def step_impl(context):
         context.resp = requests.post(rest_endpoint, json=payload)
         assert(context.resp.status_code == HTTP_201_CREATED)
 
-@when(u'I press the "{button}" button')
-def step_impl(context, button):
-    button_id = button.lower() + "-btn"
-    context.driver.find_element_by_id(button_id).click()
-
 @then('I should see the message "{message}"')
 def step_impl(context, message):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
@@ -75,7 +70,6 @@ def step_impl(context, message):
     )
     assert(found)
 
-
 @then(u'I should see "{name}" in the results')
 def step_impl(context, name):
     found = WebDriverWait(context.driver, context.wait_seconds).until(
@@ -85,7 +79,6 @@ def step_impl(context, name):
         )
     )
     assert(found)
-
 
 @then('I should not see "{name}" in the results')
 def step_impl(context, name):
